42a986f7a1f1a6849370542d22a8b8f5de89e1e0
RubikRun <borisd2000@gmail.com>
2020-05-26 23:49:32 +0300

    added Serializable interface

---
+++ Serializable/Serializable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+typedef std::string str;
+
+class Serializable
+{
+    public:
+
+        //Serializes the object to a string
+        virtual str Serializa() const = 0;
+
+        //Deserializes the object from a string
+        virtual void Deserialize(const str&) = 0;
+};
