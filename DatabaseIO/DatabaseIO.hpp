9dbebba32e3d9e477928b12b6fffc53c7768e26f
RubikRun <borisd2000@gmail.com>
2020-05-27 15:26:10 +0300

    File macros belong here

--- DatabaseIO/DatabaseIO.hpp
+++ DatabaseIO/DatabaseIO.hpp
@@ -9,3 +9,31 @@ namespace nDatabaseIO
     //Writes an object to a database
     void Write(const Serializable& obj, std::ostream& db);
 }
+
+#define CHECK_FILE_EXISTS(filename, onexist, onmissing) \
+{ \
+    std::ifstream file(filename); \
+    if (file.good()) { onexist; } \
+    else { onmissing; } \
+} \
+
+#define OPEN_OFILE(file, filename, onerror) \
+std::ofstream file(filename); \
+if (!file.is_open()) { \
+    nUserOutput::nError::Log("Cannot open file: " + filename); \
+    onerror; \
+}
+
+#define OPEN_IFILE(file, filename, onerror) \
+std::ifstream file(filename); \
+if (!file.is_open()) { \
+    nUserOutput::nError::Log("Cannot open file: " + filename); \
+    onerror; \
+}
+
+#define CLOSE_FILE(file, filename, onerror) \
+file.close(); \
+if (!file.good()) { \
+    nUserOutput::nError::Log("Cannot close file: " + filename); \
+    onerror; \
+}
