21782884e00fab916c7b7effb29c5004a6a01e29
RubikRun <borisd2000@gmail.com>
2020-05-28 19:08:07 +0300

    serializable interface class

---
+++ Core/Database/Serializable.hpp
@@ -0,0 +1,16 @@
+//An interface class for objects that can be serialized and deserialized,
+//so that we can read/write them from/to a database
+
+#pragma once
+
+#include <string>
+typedef std::string str;
+
+class Serializable
+{
+    //Serializes the object to a string
+    virtual str Serialize() const = 0;
+
+    //Deserializes a string to an object
+    virtual void Deserialize(str&) = 0;
+};
